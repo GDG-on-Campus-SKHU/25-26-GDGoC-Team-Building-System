88fd71b1d97c7adf17c6da7ebfddb7b746b35b1a
TwooTwoo <mbnman12@gmail.com>
2025-10-26 22:01:58 +0900

    init: 팀빌딩 프로젝트 스프링부트 프로젝트 생성

---
+++ src/main/java/com/skhu/gdgocteambuildingproject/GdgocTeambuildingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skhu.gdgocteambuildingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgocTeambuildingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdgocTeambuildingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skhu/gdgocteambuildingproject/GdgocTeambuildingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhu.gdgocteambuildingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgocTeambuildingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
