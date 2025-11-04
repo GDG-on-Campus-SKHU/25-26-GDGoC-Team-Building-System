93e98c3d54612d2c6d632fa4000481b2494f6b0b
bomikim0214 <rlaqhals05@naver.com>
2025-11-04 17:01:47 +0900

    feat/#1 user 제약조건 추가

--- src/main/java/com/skhu/gdgocteambuildingproject/user/domain/User.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/domain/User.java
@@ -4,6 +4,11 @@ import com.skhu.gdgocteambuildingproject.global.entity.BaseEntity;
 import com.skhu.gdgocteambuildingproject.user.domain.enumtype.UserPosition;
 import com.skhu.gdgocteambuildingproject.user.domain.enumtype.UserRole;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,11 +25,21 @@ public class User extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-]).{8,}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 합니다."
+    )
     private String password;
     private String name;
     private String number;
     private String introduction;
+    private String school;
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
@@ -36,7 +51,6 @@ public class User extends BaseEntity {
     private String generation;
     private boolean deleted;
 
-    //PERSIST
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<TechStack> techStacks = new ArrayList<>();
 
