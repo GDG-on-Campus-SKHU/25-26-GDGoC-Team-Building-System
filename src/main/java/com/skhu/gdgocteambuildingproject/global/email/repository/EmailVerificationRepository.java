c5802d22a84658f10578ac00936ba9ed8d575a6f
bomikim0214 <rlaqhals05@naver.com>
2025-11-07 00:59:43 +0900

    feat: EmailVerificationRepository

---
+++ src/main/java/com/skhu/gdgocteambuildingproject/global/email/repository/EmailVerificationRepository.java
@@ -0,0 +1,10 @@
+package com.skhu.gdgocteambuildingproject.global.email.repository;
+
+import com.skhu.gdgocteambuildingproject.global.email.domain.EmailVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+    Optional<EmailVerification> findByEmail(String email);
+}
