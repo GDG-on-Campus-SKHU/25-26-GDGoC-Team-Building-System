db5a045023d1c570aa3295f69b4ac13d36fca548
Kim, Joon <kjoon418@naver.com>
2025-11-16 12:16:53 +0900

    refactor(exception): `ExceptionMessage` 내 상수 순서 재배치

--- src/main/java/com/skhu/gdgocteambuildingproject/global/exception/ExceptionMessage.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/global/exception/ExceptionMessage.java
@@ -6,18 +6,18 @@ import lombok.Getter;
 public enum ExceptionMessage {
     // Common
     USER_NOT_EXIST("회원이 존재하지 않습니다."),
+    USER_NOT_FOUND("해당 ID의 회원을 찾을 수 없습니다."),
 
     // TeamBuilding
     SCHEDULE_NOT_EXIST("일정이 존재하지 않습니다."),
     PROJECT_NOT_EXIST("프로젝트가 존재하지 않습니다."),
-
-    USER_NOT_FOUND("해당 ID의 회원을 찾을 수 없습니다."),
-    USER_ALREADY_APPROVED("이미 승인된 회원입니다."),
-    USER_ALREADY_REJECTED("이미 거절된 회원입니다."),
-  
     IDEA_NOT_EXIST("아이디어가 존재하지 않습니다."),
     REGISTERED_IDEA_ALREADY_EXIST("등록한 아이디어가 이미 존재합니다."),
     IDEA_CONTENTS_EMPTY("내용이 비어 있습니다."),
+
+    // Admin
+    USER_ALREADY_APPROVED("이미 승인된 회원입니다."),
+    USER_ALREADY_REJECTED("이미 거절된 회원입니다."),
 
     // GalleryProject
     PROJECT_NOT_EXIST_IN_GALLERY("프로젝트가 갤러리에 존재하지 않습니다."),
