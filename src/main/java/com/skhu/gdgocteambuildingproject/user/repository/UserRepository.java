3b012188e0c0749f50c9fba5d3fa14a5feb394a4
jihoo2002 <yolohoo@naver.com>
2025-11-19 02:13:12 +0900

    feat(adminUserProfile): UserRepository 잘못된 병합 리펙토링

--- src/main/java/com/skhu/gdgocteambuildingproject/user/repository/UserRepository.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/repository/UserRepository.java
@@ -10,9 +10,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
-
     Optional<User> findByEmailAndDeletedFalse(String email);
     boolean existsByEmailAndDeletedFalse(String email);
 
