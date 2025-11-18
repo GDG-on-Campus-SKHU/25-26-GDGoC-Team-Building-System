ea1af102af1ad307752f7d5ac898c5dbdc98a52d
Park, Daekyeong <pdk2037@gmail.com>
2025-11-18 23:32:18 +0900

    fix(project gallery): 사용하지 않는 파일 삭제

--- src/main/java/com/skhu/gdgocteambuildingproject/projectgallery/dto/project/GalleryProjectFileInfoResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.skhu.gdgocteambuildingproject.projectgallery.dto.project;
-
-import lombok.Builder;
-
-@Builder
-public record GalleryProjectFileInfoResponseDto(
-        String fileUrl
-) {
-}
