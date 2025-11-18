1f69c520b82c633857c54c25c7cb2c7a69c04d26
jihoo2002 <yolohoo@naver.com>
2025-11-19 01:16:05 +0900

    feat(adminUserProfile): 승인된 모든 멤버 프로필 리스트 컨트롤러 API 작성

---
+++ src/main/java/com/skhu/gdgocteambuildingproject/admin/controller/AdminUserProfileController.java
@@ -0,0 +1,41 @@
+package com.skhu.gdgocteambuildingproject.admin.controller;
+
+import com.skhu.gdgocteambuildingproject.admin.api.AdminUserProfileApi;
+import com.skhu.gdgocteambuildingproject.admin.dto.ApproveUserInfoPageResponseDto;
+import com.skhu.gdgocteambuildingproject.admin.service.AdminUserProfileServiceImpl;
+import com.skhu.gdgocteambuildingproject.global.pagination.SortOrder;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/approved")
+@PreAuthorize("hasAnyRole('SKHU_ADMIN')")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class AdminUserProfileController implements AdminUserProfileApi {
+
+    private final String DEFAULT_PAGE = "0";
+    private final String DEFAULT_SIZE = "20";
+    private final String DEFAULT_SORT_BY = "id";
+    private final String DEFAULT_ORDER = "ASC";
+
+    private final AdminUserProfileServiceImpl adminUserProfileService;
+
+    @GetMapping("/users")
+    public ResponseEntity<ApproveUserInfoPageResponseDto> getApproveAllUsers(
+            @RequestParam(defaultValue = DEFAULT_PAGE) int page,
+            @RequestParam(defaultValue = DEFAULT_SIZE) int size,
+            @RequestParam(defaultValue = DEFAULT_SORT_BY) String sortBy,
+            @RequestParam(defaultValue = DEFAULT_ORDER) SortOrder order
+    ) {
+        ApproveUserInfoPageResponseDto approveAllUsers =
+                adminUserProfileService.getApproveAllUsers(page, size, sortBy, order);
+
+        return ResponseEntity.ok(approveAllUsers);
+    }
+}
