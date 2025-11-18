51621c8047163ee84b734e890eb29b816f5b7d8b
jihoo2002 <yolohoo@naver.com>
2025-11-19 01:44:02 +0900

    feat(adminUserProfile): userRepository 병합

--- src/main/java/com/skhu/gdgocteambuildingproject/user/repository/UserRepository.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/repository/UserRepository.java
@@ -1,11 +1,22 @@
 package com.skhu.gdgocteambuildingproject.user.repository;
 
 import com.skhu.gdgocteambuildingproject.user.domain.User;
+import com.skhu.gdgocteambuildingproject.user.domain.enumtype.ApprovalStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmailAndDeletedFalse(String email);
+    boolean existsByEmailAndDeletedFalse(String email);
+
+    Page<User> findAllByApprovalStatus(ApprovalStatus approvalStatus, Pageable pageable);
+
+    List<User> findByNameContaining(String query);
 }
