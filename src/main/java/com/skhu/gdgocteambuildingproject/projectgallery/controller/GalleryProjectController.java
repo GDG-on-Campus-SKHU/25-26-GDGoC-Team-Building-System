abc9d4bedfb5aeba2ea9fa16bd8230a23f556d88
Park, Daekyeong <pdk2037@gmail.com>
2025-11-19 23:10:14 +0900

    refactor(project gallery): `PATCH` 메서드를 `PUT`으로 변경

--- src/main/java/com/skhu/gdgocteambuildingproject/projectgallery/controller/GalleryProjectController.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/projectgallery/controller/GalleryProjectController.java
@@ -13,9 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -102,7 +102,7 @@ public class GalleryProjectController {
         return ResponseEntity.ok(galleryProjectService.searchMemberByName(name));
     }
 
-    @PatchMapping("/{projectId}")
+    @PutMapping("/{projectId}")
     @Operation(
             summary = "프로젝트 갤러리에 존재하는 프로젝트의 정보 수정",
             description =
