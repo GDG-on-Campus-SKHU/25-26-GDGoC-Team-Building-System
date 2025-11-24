7642915060d15ad03c9f6a66b9d6e4b1478d4949
Kim, Joon <kjoon418@naver.com>
2025-11-24 13:55:53 +0900

    feat(domain): 안전한 동등 비교를 위해 BaseEntity에 `id` 기반 `equals`, `hashCode` 재정의

--- src/main/java/com/skhu/gdgocteambuildingproject/global/entity/BaseEntity.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/global/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.skhu.gdgocteambuildingproject.global.entity;
 
 import jakarta.persistence.*;
+import java.util.Objects;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,4 +21,20 @@ public abstract class BaseEntity {
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
