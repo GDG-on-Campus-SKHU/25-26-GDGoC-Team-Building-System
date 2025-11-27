a1fa01fdd0017a3187ff8c50c21c771fe77c2ab6
jihoo2002 <yolohoo@naver.com>
2025-11-27 22:05:36 +0900

    feat(adminActivity): ActivityRepository에 카테고리별 Activity count 메서드 추가

--- src/main/java/com/skhu/gdgocteambuildingproject/community/repository/ActivityRepository.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/community/repository/ActivityRepository.java
@@ -1,7 +1,13 @@
 package com.skhu.gdgocteambuildingproject.community.repository;
 
 import com.skhu.gdgocteambuildingproject.community.domain.Activity;
+import com.skhu.gdgocteambuildingproject.community.domain.ActivityCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    long countByActivityCategory(ActivityCategory activityCategory);
+
+    List<Activity> findByActivityCategory(ActivityCategory category);
 }
