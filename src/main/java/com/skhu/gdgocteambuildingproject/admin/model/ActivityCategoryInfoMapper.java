adabfc9af22a4a9dda4638519101478a1383c1da
jihoo2002 <yolohoo@naver.com>
2025-11-27 21:46:34 +0900

    feat(adminActivity): 액티비티 맵퍼용 클래스 추가

---
+++ src/main/java/com/skhu/gdgocteambuildingproject/admin/model/ActivityCategoryInfoMapper.java
@@ -0,0 +1,18 @@
+package com.skhu.gdgocteambuildingproject.admin.model;
+
+import com.skhu.gdgocteambuildingproject.admin.dto.activity.ActivityCategoryInfoResponseDto;
+import com.skhu.gdgocteambuildingproject.community.domain.ActivityCategory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActivityCategoryInfoMapper {
+    public ActivityCategoryInfoResponseDto toActivityCategoryInfoResponseDto(ActivityCategory category,
+                                                                             Long count) {
+        return ActivityCategoryInfoResponseDto.builder()
+                .categoryId(category.getId())
+                .categoryName(category.getName())
+                .isPublished(category.isPublished())
+                .count(count)
+                .build();
+    }
+}
