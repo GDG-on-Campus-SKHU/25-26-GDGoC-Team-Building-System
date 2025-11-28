d10c2a845193c6c449b14a341735988871f93a38
jihoo2002 <yolohoo@naver.com>
2025-11-28 13:34:26 +0900

    feat(adminProjectGallery): projectGallery 엔터티를 dto로 변환하는 매퍼 클래스 및 dto 추가

---
+++ src/main/java/com/skhu/gdgocteambuildingproject/admin/dto/projectGallery/ProjectGalleryResponseDto.java
@@ -0,0 +1,16 @@
+package com.skhu.gdgocteambuildingproject.admin.dto.projectGallery;
+
+import com.skhu.gdgocteambuildingproject.projectgallery.domain.enumtype.ServiceStatus;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record ProjectGalleryResponseDto(
+        Long id,
+        String projectName,
+        String generation,
+        ServiceStatus serviceStatus,
+        LocalDateTime createdAt
+) {
+}
