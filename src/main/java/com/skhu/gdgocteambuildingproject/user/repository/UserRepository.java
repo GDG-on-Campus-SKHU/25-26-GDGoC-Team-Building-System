38498ceaf91b46d3829d93970365961be2aa748e
jihoo2002 <yolohoo@naver.com>
2025-12-01 14:41:55 +0900

    feat(adminUserProfile): 승인된 사용자만 검색하도록 Repository 메서드 추가

--- src/main/java/com/skhu/gdgocteambuildingproject/user/repository/UserRepository.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.skhu.gdgocteambuildingproject.user.repository;
 
+import com.skhu.gdgocteambuildingproject.global.enumtype.Part;
 import com.skhu.gdgocteambuildingproject.user.domain.User;
 import com.skhu.gdgocteambuildingproject.user.domain.enumtype.ApprovalStatus;
 import org.springframework.data.domain.Page;
@@ -11,9 +12,28 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailAndDeletedFalse(String email);
+
     boolean existsByEmailAndDeletedFalse(String email);
 
     Page<User> findAllByApprovalStatus(ApprovalStatus approvalStatus, Pageable pageable);
 
     List<User> findByNameContaining(String query);
+
+    Page<User> findByNameContainingAndApprovalStatus(
+            String query,
+            ApprovalStatus status,
+            Pageable pageable
+    );
+
+    Page<User> findByPartAndApprovalStatus(
+            Part part,
+            ApprovalStatus status,
+            Pageable pageable
+    );
+
+    Page<User> findBySchoolContainingAndApprovalStatus(
+            String school,
+            ApprovalStatus status,
+            Pageable pageable
+    );
 }
