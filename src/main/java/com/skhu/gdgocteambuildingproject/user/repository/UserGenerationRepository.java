766acee74a669e2d2b95aa44f8cf9eea51bd1cc7
jihoo2002 <yolohoo@naver.com>
2025-12-04 16:55:25 +0900

    refactor(domain): UserGeneration 레파지토리 추가

---
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/repository/UserGenerationRepository.java
@@ -0,0 +1,7 @@
+package com.skhu.gdgocteambuildingproject.user.repository;
+
+import com.skhu.gdgocteambuildingproject.user.domain.UserGeneration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserGenerationRepository extends JpaRepository<UserGeneration, Long> {
+}
