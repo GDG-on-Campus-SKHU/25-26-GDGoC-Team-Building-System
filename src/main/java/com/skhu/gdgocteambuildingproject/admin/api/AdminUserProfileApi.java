438ed1a8d2c35f0221973b0357c9cf5ffa0c6e27
jihoo2002 <yolohoo@naver.com>
2025-12-07 22:11:23 +0900

    refactor(AdminUserProfile) : 병합하면서 누락된 import 추가

--- src/main/java/com/skhu/gdgocteambuildingproject/admin/api/AdminUserProfileApi.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/admin/api/AdminUserProfileApi.java
@@ -8,6 +8,7 @@ import com.skhu.gdgocteambuildingproject.global.enumtype.Part;
 import com.skhu.gdgocteambuildingproject.global.pagination.SortOrder;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
