3c8f25b08a4cafa72e11db370d0e0499071d4d2e
jihoo2002 <yolohoo@naver.com>
2025-12-08 02:44:34 +0900

    refactor(mapper) : GenerationMapper로 리펙토링

--- src/main/java/com/skhu/gdgocteambuildingproject/admin/model/UserInfoMapper.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/admin/model/UserInfoMapper.java
@@ -1,31 +1,27 @@
 package com.skhu.gdgocteambuildingproject.admin.model;
 
 import com.skhu.gdgocteambuildingproject.admin.dto.UserResponseDto;
+import com.skhu.gdgocteambuildingproject.admin.util.GenerationMapper;
 import com.skhu.gdgocteambuildingproject.user.domain.User;
-import com.skhu.gdgocteambuildingproject.user.domain.enumtype.Generation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
+@RequiredArgsConstructor
 public class UserInfoMapper {
+
+    private final GenerationMapper generationMapper;
+
     public UserResponseDto toUserResponseDto(User user) {
         return UserResponseDto.builder()
                 .id(user.getId())
                 .userName(user.getName())
                 .part(user.getPart())
-                .generation(mapGenerations(user))
+                .generations(generationMapper.toMainGenerationDtos(user))
                 .school(user.getSchool())
                 .approvalStatus(user.getApprovalStatus())
                 .email(user.getEmail())
                 .number(user.getNumber())
                 .build();
-    }
-
-    private List<String> mapGenerations(User user) {
-        return user.getGeneration()
-                .stream()
-                .map(gen -> gen.getGeneration().getLabel())
-                .toList();
     }
 }
