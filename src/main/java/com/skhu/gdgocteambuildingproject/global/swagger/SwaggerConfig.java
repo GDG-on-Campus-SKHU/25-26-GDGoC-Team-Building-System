729652c83991a4ccd7b8e5cca4d688fb625ff460
bomikim0214 <rlaqhals05@naver.com>
2025-12-08 00:12:40 +0900

    fix: swaggerconfig 파일 재제출

--- src/main/java/com/skhu/gdgocteambuildingproject/global/swagger/SwaggerConfig.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/global/swagger/SwaggerConfig.java
@@ -5,17 +5,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerConfig {
 
     private static final String BEARER_KEY = "bearerAuth";
 
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
+    public OpenAPI openAPI(SwaggerProperties swaggerProperties) {
+        OpenAPI openAPI = new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes(BEARER_KEY,
                                 new SecurityScheme()
@@ -27,6 +30,12 @@ public class SwaggerConfig {
                 )
                 .addSecurityItem(new SecurityRequirement().addList(BEARER_KEY))
                 .info(apiInfo());
+
+        swaggerProperties.servers().forEach(url ->
+                openAPI.addServersItem(new Server().url(url))
+        );
+
+        return openAPI;
     }
 
     private Info apiInfo() {
