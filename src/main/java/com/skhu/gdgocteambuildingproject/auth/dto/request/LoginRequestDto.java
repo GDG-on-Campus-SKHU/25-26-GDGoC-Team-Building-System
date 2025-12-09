4a4f7cdbde7cfcbe7276df82ea7159b879229d0f
bomikim0214 <rlaqhals05@naver.com>
2025-12-10 03:31:41 +0900

    chore: 회원가입시 제약조건 설정 추가

--- src/main/java/com/skhu/gdgocteambuildingproject/auth/dto/request/LoginRequestDto.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/auth/dto/request/LoginRequestDto.java
@@ -1,7 +1,7 @@
 package com.skhu.gdgocteambuildingproject.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
@@ -10,6 +10,9 @@ public class LoginRequestDto {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,}$",
+            message = "비밀번호는 8자 이상이며 특수문자를 1개 이상 포함해야 합니다."
+    )
     private String password;
 }
