6722ce5c4902666227c255277403aeecb36c334d
bomikim0214 <rlaqhals05@naver.com>
2025-12-13 01:29:24 +0900

    feat: UserPosition, UserRole 예외처리 추가

--- src/main/java/com/skhu/gdgocteambuildingproject/user/domain/enumtype/UserPosition.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/domain/enumtype/UserPosition.java
@@ -1,5 +1,17 @@
 package com.skhu.gdgocteambuildingproject.user.domain.enumtype;
 
+import com.skhu.gdgocteambuildingproject.global.exception.ExceptionMessage;
+
 public enum UserPosition {
-    MEMBER, CORE, ORGANIZER
+    MEMBER, CORE, ORGANIZER;
+
+    public static UserPosition from(String value) {
+        try {
+            return UserPosition.valueOf(value);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new IllegalArgumentException(
+                    ExceptionMessage.INVALID_USER_POSITION.getMessage()
+            );
+        }
+    }
 }
--- src/main/java/com/skhu/gdgocteambuildingproject/user/domain/enumtype/UserRole.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/domain/enumtype/UserRole.java
@@ -1,5 +1,17 @@
 package com.skhu.gdgocteambuildingproject.user.domain.enumtype;
 
+import com.skhu.gdgocteambuildingproject.global.exception.ExceptionMessage;
+
 public enum UserRole {
-    SKHU_ADMIN, SKHU_MEMBER, OTHERS
+    SKHU_ADMIN, SKHU_MEMBER, OTHERS;
+
+    public static UserRole from(String value) {
+        try {
+            return UserRole.valueOf(value);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new IllegalArgumentException(
+                    ExceptionMessage.INVALID_USER_ROLE.getMessage()
+            );
+        }
+    }
 }
