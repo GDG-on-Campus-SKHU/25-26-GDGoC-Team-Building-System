b7459917b3592ce94386d694f559f5e0816fd1c8
bomikim0214 <rlaqhals05@naver.com>
2025-12-13 00:29:41 +0900

    refactor: swagger password 예시 수정

--- src/main/java/com/skhu/gdgocteambuildingproject/auth/dto/request/LoginRequestDto.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/auth/dto/request/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.skhu.gdgocteambuildingproject.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -8,11 +9,13 @@ import lombok.Getter;
 public class LoginRequestDto {
 
     @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Schema(example = "test@example.com")
     private String email;
 
     @Pattern(
             regexp = "^(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,}$",
             message = "비밀번호는 8자 이상이며 특수문자를 1개 이상 포함해야 합니다."
     )
+    @Schema(example = "test1234!")
     private String password;
 }
