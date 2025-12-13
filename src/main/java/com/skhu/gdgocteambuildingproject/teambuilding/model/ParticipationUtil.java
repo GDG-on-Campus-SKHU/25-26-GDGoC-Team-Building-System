fa40cd7f7b3c9eecd8e1300e22d7e14386b1d9d1
Kim, Joon <kjoon418@naver.com>
2025-12-13 22:52:10 +0900

    feat(team building): 관리자는 프로젝트 참여 여부 검증을 건너뛰도록 개선

--- src/main/java/com/skhu/gdgocteambuildingproject/teambuilding/model/ParticipationUtil.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/teambuilding/model/ParticipationUtil.java
@@ -1,8 +1,12 @@
 package com.skhu.gdgocteambuildingproject.teambuilding.model;
 
 import static com.skhu.gdgocteambuildingproject.global.exception.ExceptionMessage.NOT_PARTICIPATED;
+import static com.skhu.gdgocteambuildingproject.global.exception.ExceptionMessage.USER_NOT_EXIST;
 
 import com.skhu.gdgocteambuildingproject.teambuilding.repository.ProjectParticipantRepository;
+import com.skhu.gdgocteambuildingproject.user.domain.User;
+import com.skhu.gdgocteambuildingproject.user.domain.enumtype.UserRole;
+import com.skhu.gdgocteambuildingproject.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,16 +15,28 @@ import org.springframework.stereotype.Component;
 public class ParticipationUtil {
 
     private final ProjectParticipantRepository participantRepository;
+    private final UserRepository userRepository;
 
     public boolean isParticipated(long userId, long projectId) {
         return participantRepository.existsByUserIdAndProjectId(userId, projectId);
     }
 
     public void validateParticipation(long userId, long projectId) {
+        if (isAdmin(userId)) {
+            return;
+        }
+
         boolean participated = participantRepository.existsByUserIdAndProjectId(userId, projectId);
 
         if (!participated) {
             throw new IllegalStateException(NOT_PARTICIPATED.getMessage());
         }
+    }
+
+    private boolean isAdmin(long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalStateException(USER_NOT_EXIST.getMessage()));
+
+        return user.getRole() == UserRole.ROLE_SKHU_ADMIN;
     }
 }
