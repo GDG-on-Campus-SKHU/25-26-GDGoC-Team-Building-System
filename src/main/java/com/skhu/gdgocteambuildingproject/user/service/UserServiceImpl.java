8d930a8c6524249d4286c0bdfd50c643aecf8ceb
Kim, Joon <kjoon418@naver.com>
2025-12-24 12:38:37 +0900

    fix(auth): 프로젝트가 존재하지 않을 때 예외를 던지지 않도록 수정

--- src/main/java/com/skhu/gdgocteambuildingproject/user/service/UserServiceImpl.java
+++ src/main/java/com/skhu/gdgocteambuildingproject/user/service/UserServiceImpl.java
@@ -1,20 +1,15 @@
 package com.skhu.gdgocteambuildingproject.user.service;
 
 import com.skhu.gdgocteambuildingproject.global.util.PrincipalUtil;
-import com.skhu.gdgocteambuildingproject.teambuilding.domain.TeamBuildingProject;
 import com.skhu.gdgocteambuildingproject.teambuilding.model.ProjectUtil;
 import com.skhu.gdgocteambuildingproject.teambuilding.repository.ProjectParticipantRepository;
 import com.skhu.gdgocteambuildingproject.user.domain.User;
 import com.skhu.gdgocteambuildingproject.user.dto.UserIdResponseDto;
-import jakarta.persistence.EntityNotFoundException;
+import java.security.Principal;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.security.Principal;
-
-import static com.skhu.gdgocteambuildingproject.global.exception.ExceptionMessage.PROJECT_NOT_EXIST;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
@@ -33,9 +28,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean isUserParticipated(User user) {
-        TeamBuildingProject currentProject = projectUtil.findCurrentProject()
-                .orElseThrow(() -> new EntityNotFoundException(PROJECT_NOT_EXIST.getMessage()));
-
-        return projectParticipantRepository.existsByUserIdAndProjectId(user.getId(), currentProject.getId());
+        return projectUtil.findCurrentProject()
+                .map(currentProject -> projectParticipantRepository.existsByUserIdAndProjectId(user.getId(), currentProject.getId()))
+                .orElse(false);
     }
 }
